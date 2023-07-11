be7fd3caef42326fe87a0cc25f9ab0a74f44ee10
kimsungmin1011 <tata153@naver.com>
2023-07-11 15:49:21 +0900

    d- -> 까지

--- src/App.js
+++ src/App.js
@@ -291,7 +291,7 @@ function Card(props) {
         <img src={props.shoes.img} />
         <h6 className='ply'>{props.shoes.companyName}</h6>
         <h4 className='ply'>{props.shoes.title}</h4><br /><br />
-        <p className='ply1'>D-{props.shoes.expDate}</p>
+        <p className='ply1'>{props.shoes.expDate}까지</p>
       </div></div>
   )
 }
